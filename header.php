c6b2553f9f8db4e69ae944e1654a971a3687ffe5
dharmendersingh1 <125878499+dharmendersingh1@users.noreply.github.com>
2026-03-30 04:05:08 -0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Builderz - Construction Company Website Template</title>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <meta content="Construction Company Website Template" name="keywords">
+        <meta content="Construction Company Website Template" name="description">
+
+        <!-- Favicon -->
+        <!-- <link href="img/favicon.ico" rel="icon"> -->
+
+        <!-- Google Font -->
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+        <!-- CSS Libraries -->
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+        <link href="lib/flaticon/font/flaticon.css" rel="stylesheet"> 
+        <link href="lib/animate/animate.min.css" rel="stylesheet">
+        <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+        <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+        <link href="lib/slick/slick.css" rel="stylesheet">
+        <link href="lib/slick/slick-theme.css" rel="stylesheet">
+
+        <!-- Template Stylesheet -->
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+
+    <body>
+
+
+<!-- <<<<<<<<<< navbaaar start>>>>>>>>>>> -->
+
+
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top navbar-custom" id="mainNavbar">
+  <div class="container">
+
+    <!-- LOGO -->
+    <a class="navbar-brand" href="#">
+      <img src="img/malogo.png"  alt="Logo" class="img-fluid logo-img ">
+    </a>
+
+    <!-- TOGGLER -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- MENU -->
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto text-center">
+
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="about.php">About</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="service.php">Services</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="project.php">Project</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="contact.php">Contact Us</a>
+        </li>
+
+      </ul>
+    </div>
+
+  </div>
+</nav>
+<!-- <<<<<<<<<<<<<<<< navbaar end>>>>>>>>>>>>> -->
+
+
+
+
+
+
+
+
+
+
+
+     <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+        </div>
+
+        <!-- JavaScript Libraries -->
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+        <script src="lib/easing/easing.min.js"></script>
+        <script src="lib/wow/wow.min.js"></script>
+        <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+        <script src="lib/isotope/isotope.pkgd.min.js"></script>
+        <script src="lib/lightbox/js/lightbox.min.js"></script>
+        <script src="lib/waypoints/waypoints.min.js"></script>
+        <script src="lib/counterup/counterup.min.js"></script>
+        <script src="lib/slick/slick.min.js"></script>
+
+        <!-- Template Javascript -->
+        <script src="js/main.js"></script>
+
+       <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+            <!-- Nav Bar End -->
+
+<script>
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 50) {
+    $('.navbar-custom').addClass('navbar-scrolled');
+  } else {
+    $('.navbar-custom').removeClass('navbar-scrolled');
+  }
+});
+</script>
+
+
+
+<script>
+function toggle(el){
+    el.classList.toggle("active");
+}
+</script>
+
+
+<!-- <<<<<nav>>>>>>>>> -->
+  <script>
+window.addEventListener("scroll", function () {
+    var navbar = document.getElementById("mainNavbar");
+
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
+</script>
+
+
+
+
+  </body>
+</html>
+
+
+
+
